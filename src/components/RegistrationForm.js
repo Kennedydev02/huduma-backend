87f7ba79fa878940d5685485d2b8748c1f0ce1c9
kennedydev02 <your-email@example.com>
2024-12-27 01:43:46 -0800

    Add mobile-responsive fixed layout

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const RegistrationForm = () => {
+  return (
+    <div className="form-container">
+      {/* Your existing form content */}
+    </div>
+  );
+};
+
+export default RegistrationForm; 
